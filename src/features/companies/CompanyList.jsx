5f68f048d1bbae4c779bb9fd8e81863c47ac50cf
anhlb-0396 <le.bao.anh@sun-asterisk.com>
2024-04-09 16:52:14 +0700

    [Companies List] Add pagination for list

--- src/features/companies/CompanyList.jsx
+++ src/features/companies/CompanyList.jsx
@@ -1,14 +1,28 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import TitleText from "../../ui/inputs/TitleText";
 import CompanySummaryCard from "./CompanySummaryCard";
 import { useCompanies } from "./useCompanies";
 import { Grid } from "@mui/material";
+import AppPagination from "../../ui/AppPagination";
+
+const COMPANIES_PER_PAGE = 3;
 
 function CompanyList() {
   const { companies, isLoading, isError, error } = useCompanies();
+  const [currentPage, setCurrentPage] = useState(1);
 
   if (isLoading) return <div>Loading...</div>;
   if (isError) return <div>Error: {error.message}</div>;
-  console.log(companies);
+
+  const startIndex = (currentPage - 1) * COMPANIES_PER_PAGE;
+  const endIndex = currentPage * COMPANIES_PER_PAGE;
+  const paginatedCompanies = companies.slice(startIndex, endIndex);
+  const count = Math.ceil(companies.length / COMPANIES_PER_PAGE);
+
+  const handleChange = (event, value) => {
+    setCurrentPage(value);
+  };
 
   return (
     <Grid
@@ -23,7 +37,7 @@ function CompanyList() {
         <TitleText variant="h4">Danh sách các công ty nổi bật</TitleText>
       </Grid>
 
-      {companies?.map((company) => (
+      {paginatedCompanies?.map((company) => (
         <CompanySummaryCard
           key={company.id}
           company={company}
@@ -31,6 +45,12 @@ function CompanyList() {
           md={4}
         ></CompanySummaryCard>
       ))}
+
+      <AppPagination
+        onChange={handleChange}
+        currentPage={currentPage}
+        count={count}
+      ></AppPagination>
     </Grid>
   );
 }
